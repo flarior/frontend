ab463bac873bc9cc3b7a04091ffd02e3ab772944
Veljko Simic <veljko.simic98@gmail.com>
2015-09-05 22:07:10 +0200

    Transform jsx to js in gulp

---
+++ app/js/components/Locations.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Locations = React.createClass({
+    render() {
+        return (
+            <h1>Uspelo</h1>
+        );
+    }
+});
+
+module.exports = Locations;
--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,23 @@ var path  = require('path');
 
 global.gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var browserify = require('gulp-browserify');
 var sass = require('gulp-sass');
+var useref = require('gulp-useref');
+var rename = require('gulp-rename');
+var reactify = require('gulp-reactify');
+var source = require('vinyl-source-stream');
 
 global.flariorPaths = {
   app : path.resolve('./app/'),
   sass : path.resolve('./app/sass'),
   sassGlob: path.resolve('./app/sass/**/*.scss'),
+  javascript: path.resolve('./app/js/**/*.js*'),
+  jsxEntry: path.resolve('./app/js/app.jsx'),
   css: path.resolve('./app/css')
 }
 
-gulp.task('webserver',['sass', 'watch'], function() {
+gulp.task('webserver',['sass', 'watch', 'compileJs'], function() {
   gulp.src(flariorPaths.app)
     .pipe(webserver({
       host: '0.0.0.0',
@@ -27,11 +34,15 @@ gulp.task('sass', function compileInuit(){
   .pipe(sass({
     errLogToConsole: true
   }))
-  .pipe(gulp.dest('./app/css'))
+  .pipe(gulp.dest('./app/static/css'))
+});
+
+gulp.task('compileJs', function(){
 });
 
 gulp.task('watch', function(){
   gulp.watch(flariorPaths.sassGlob, ['sass']);
+  gulp.watch(flariorPaths.javascript, ['compileJs']);
 });
 
 gulp.task('default', ['webserver']);

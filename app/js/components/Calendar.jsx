a03c9c6b5bc2566b004fb2ae7462dec649d1cf7a
Goran Mekić <meka@lugons.org>
2015-09-06 05:34:38 +0200

    Not even a week view

--- app/js/components/Calendar.jsx
+++ app/js/components/Calendar.jsx
@@ -1,27 +1,81 @@
 var React = require('react');
 var Day = require('./Day.jsx')
 
-var days = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-  'Sunday'
-];
-
 var Calendar = React.createClass({
-    render() {
-        return (
-          <div className="calendar">
-            <h1>My first react app</h1>
-            {days.map((day) => {
-              return <Day day={day} classes={"day " + day}/>
-            })}
-          </div>
-        );
-    }
+  getInitialState: function() {
+    var current_date = new Date();
+    var month_names = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+    var current_month_number = current_date.getMonth();
+    var current_month_name = month_names[current_month_number];
+    var days_in_week = [
+      {
+        'name': 'Monday',
+        'number': 1
+      },
+      {
+        'name': 'Tuesday',
+        'number': 2,
+      },
+      {
+        'name': 'Wednesday',
+        'number': 3,
+      },
+      {
+        'name': 'Thursday',
+        'number': 4,
+      },
+      {
+        'name': 'Friday',
+        'number': 5,
+      },
+      {
+        'name': 'Saturday',
+        'number': 6,
+      },
+      {
+        'name': 'Sunday',
+        'number': 0,
+      }
+    ];
+    return {
+      days: days_in_week,
+      current_month_name: current_month_name,
+      current_month_number: current_month_number,
+    };
+  },
+
+  current_day_number: new Date().getDay(),
+
+  render: function() {
+    return (
+      <div className="calendar">
+        <h1>{this.state.current_month_name}</h1>
+        {this.state.days.map((day) => {
+          var classes = "u-1/7 text--center day day-number-" + day.number + " day-" + day.name;
+          if (this.current_day_number === day.number) {
+            classes += " current";
+          }
+          if (day.number === 6 || day.number === 0) {
+            classes += " weekend"
+          }
+          return <Day day={day.name} classes={classes}/>
+        })}
+      </div>
+    );
+  }
 });
 
 module.exports = Calendar;
--- app/js/components/Day.jsx
+++ app/js/components/Day.jsx
@@ -2,7 +2,6 @@ var React = require('react');
 
 var Day = React.createClass({
     render() {
-      var class_list = "day " + "{this.props.day}";
         return (
           <div className={this.props.classes}>
             {this.props.day}

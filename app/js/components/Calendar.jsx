ec5e261a67b0fc4525f9c71453ad5bfd03076004
Goran Mekić <meka@lugons.org>
2015-09-07 11:45:58 +0200

    Add hours to the left

--- app/js/components/Calendar.jsx
+++ app/js/components/Calendar.jsx
@@ -1,6 +1,16 @@
 var React = require('react');
 var Day = require('./Day.jsx')
 
+var Hours = React.createClass({
+  render: function() {
+    return (
+      <div className="content text--center u-1/40">
+        <div className="content-item">sati</div>
+      </div>
+    );
+  }
+});
+
 var Calendar = React.createClass({
   getInitialState: function() {
     var current_date = new Date();
@@ -57,22 +67,29 @@ var Calendar = React.createClass({
     };
   },
 
-  current_day_number: new Date().getDay(),
+  current_day_number: null,
+
+  componentWillMount: function() {
+    this.current_day_number = new Date().getDay();
+  },
 
   render: function() {
     return (
       <div className="calendar">
         <h1>{this.state.current_month_name}</h1>
-        {this.state.days.map((day) => {
-          var classes = "u-1/7 text--center day day-number-" + day.number + " day-" + day.name;
-          if (this.current_day_number === day.number) {
-            classes += " current";
-          }
-          if (day.number === 6 || day.number === 0) {
-            classes += " weekend"
-          }
-          return <Day day={day.name} classes={classes}/>
-        })}
+        <Hours />
+        <div className="u-39/40 content">
+          {this.state.days.map((day) => {
+            var classes = "u-1/7 text--center content-item";
+            if (this.current_day_number === day.number) {
+              classes += " current";
+            }
+            if (day.number === 6 || day.number === 0) {
+              classes += " weekend"
+            }
+            return <Day day={day.name} classes={classes}/>
+          })}
+        </div>
       </div>
     );
   }

70ea991226d539e1276952a89aa0b6cb2f39600d
Goran Mekić <meka@lugons.org>
2015-09-21 09:08:26 +0200

    Add offpage menu

--- app/js/components/Menu.jsx
+++ app/js/components/Menu.jsx
@@ -2,13 +2,14 @@ var React = require('react');
 
 var Menu = React.createClass({
     render() {
-        return (
-          <div className="breadcrumb">
-            <a href="#">Day</a>
-            <a href="#" className="active">Week</a>
-            <a href="#">Month</a>
-          </div>
-        );
+      return (
+        <div className="breadcrumb">
+          <a href="#">Day</a>
+          <a href="#" className="active">Week</a>
+          <a href="#">Month</a>
+          <a href="#navigation" className="navigation-menu active">Nav</a>
+        </div>
+      );
     }
 });
 
---
+++ app/js/components/Navigation.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Menu = React.createClass({
+    render() {
+      return (
+        <nav id="navigation" className="navigation">
+          <a href="#">x</a>
+        </nav>
+      );
+    }
+});
+
+module.exports = Menu;

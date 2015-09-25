6923c90a060496b7bb5f887d50eb11ab20db39c3
Veljko Simic <veljko.simic98@gmail.com>
2015-09-25 10:21:13 +0200

    Show date in Title

--- app/js/components/Title.jsx
+++ app/js/components/Title.jsx
@@ -1,17 +1,34 @@
 var React = require('react');
+var moment  = require('moment');
 
 var Title = React.createClass({
+  getInitialState: function(){
+    var today = moment()
+    var startWeek = moment([today.year(), today.month(), today.date() - today.day()+1])
+    var endWeek = moment([today.year(), today.month(), startWeek.date()+6])
+    return {
+      start: startWeek,
+      end: endWeek
+    };
+  },
+  nextWeek: function(event){
+    this.setState({start: start.day(7), end: end.day(7)})
+  },
+  previousWeek: function(event){
+    this.setState({start: start.day(-7), end: end.day(-7)})
+  },
+
     render() {
         return (
           <div className="title">
             <div className="u-1/3 title--item title--previous">
-              <a href="#">&lt;</a>
+              <a href="" onClick={this.previousWeek}>&lt;</a>
             </div>
             <div className="u-1/3 title--item title--data">
-              <h3>September</h3>
+              <h3>{this.state.start.format("D MMM")} - {this.state.end.format("D MMM")}</h3>
             </div>
             <div className="u-1/3 title--item title--next">
-              <a href="#">&gt;</a>
+              <a href="" onClick={this.nextWeek}>&gt;</a>
             </div>
           </div>
         );
